5a169e397c9d66ef1114506bb6dd78e62f8e99cc
Alexander Sokol <sokol@compatibl.com>
2023-09-16 13:13:40 -0400

    Add linter and formatter settings and script.

---
+++ scripts/format.cmd
@@ -0,0 +1,20 @@
+@echo off
+
+pushd ..
+
+echo.
+echo Format using isort
+isort confirms --sp=.isort.cfg
+isort tests --sp=.isort.cfg
+
+echo.
+echo Format using black
+black -q confirms --config=pyproject.toml
+black -q tests --config=pyproject.toml
+
+echo.
+echo Validate using flake8
+flake8 confirms --config=.flake8
+flake8 tests --config=.flake8
+
+popd
