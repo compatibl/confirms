a63b249c4e3ac23a770ae7c0af94cdb411aab8ca
Kateryna Dudko <katerynad@compatibl.com>
2023-09-21 04:10:52 -0700

    Add PromptTemplate.

--- tests/general/test_riddles.py
+++ tests/general/test_riddles.py
@@ -17,6 +17,7 @@ from typing import Optional
 
 import pandas as pd
 import pytest
+from langchain import PromptTemplate
 
 from confirms.core.llm.gpt_native_llm import GptNativeLlm
 from confirms.core.llm.llama_lang_chain_llm import LlamaLangChainLlm
@@ -25,30 +26,29 @@ from confirms.core.llm.llama_lang_chain_llm import LlamaLangChainLlm
 def run_sally_riddle(*, result_name: str, temperature: Optional[float] = None):
     """Run for solving Sally and her siblings riddle."""
 
+    context = "```Sally has three brothers. Each of Sally's brothers has two sisters.```"
+    template = (
+        "<s>[INST] Pay attention and remember information below, which will help to answer the question "
+        "or imperative after the context ends. "
+        "Context: {context}. "
+        "According to only the information in the document sources provided within the context above, "
+        "how many sisters does Sally have? [/INST]"
+    )
+
     results = []
     for seed in range(1, 26):
         cur_result = {}
         model_types = ["gpt-3.5-turbo", "gpt-4", "llama-2-7b-chat.Q4_K_M.gguf", "llama-2-13b-chat.Q4_K_M.gguf"]
         for model_type in model_types:
-            # TODO: Implement prompt
-            question = "```Sally has three brothers. Each of Sally's brothers has two sisters.```"
-            request = (
-                "<s>[INST] Pay attention and remember information below, which will help to answer the question "
-                "or imperative after the context ends. "
-                f"Context: {question}. "
-                f"According to only the information in the document sources provided within the context above, "
-                f"how many sisters does Sally have? [/INST]"
-            )
-
             if model_type.startswith("llama"):
                 llm = LlamaLangChainLlm(model_type=model_type, temperature=temperature, seed=seed)
-                # , grammar_file="frequency.gbnf")
             elif model_type.startswith("gpt"):
                 llm = GptNativeLlm(model_type=model_type, temperature=temperature)
             else:
                 raise RuntimeError(f"Unknown model type: {model_type}")
 
-            answer = llm.completion(request)
+            prompt = PromptTemplate(template=template, input_variables=["context"])
+            answer = llm.completion(context, prompt=prompt)
             cur_result[model_type] = answer
         results.append(pd.DataFrame([cur_result]))
 
@@ -62,33 +62,32 @@ def run_sally_riddle(*, result_name: str, temperature: Optional[float] = None):
 def run_apples_riddle(*, result_name: str, temperature: Optional[float] = None):
     """Test for solving apples in a box riddle."""
 
+    context = (
+        "```A green apple is in the same box as three red apples. "
+        "Each of these three red apples is in the same box as two green apples.```"
+    )
+    template = (
+        "<s>[INST] Pay attention and remember information below, "
+        "which will help to answer the question or imperative after the context ends. "
+        "Context: {context}. "
+        "According to only the information in the document sources provided within the context above, "
+        "how many other green apples are in the same box as the original green apple? [/INST]"
+    )
+
     results = []
     for seed in range(1, 26):
         cur_result = {}
         model_types = ["gpt-3.5-turbo", "gpt-4", "llama-2-7b-chat.Q4_K_M.gguf", "llama-2-13b-chat.Q4_K_M.gguf"]
         for model_type in model_types:
-            # TODO: Implement prompt
-            question = (
-                "```A green apple is in the same box as three red apples. "
-                "Each of these three red apples is in the same box as two green apples.```"
-            )
-            request = (
-                "<s>[INST] Pay attention and remember information below, "
-                "which will help to answer the question or imperative after the context ends. "
-                f"Context: {question}. "
-                f"According to only the information in the document sources provided within the context above, "
-                f"how many other green apples are in the same box as the original green apple? [/INST]"
-            )
-
             if model_type.startswith("llama"):
                 llm = LlamaLangChainLlm(model_type=model_type, temperature=temperature, seed=seed)
-                # , grammar_file="frequency.gbnf")
             elif model_type.startswith("gpt"):
                 llm = GptNativeLlm(model_type=model_type, temperature=temperature)
             else:
                 raise RuntimeError(f"Unknown model type: {model_type}")
 
-            answer = llm.completion(request)
+            prompt = PromptTemplate(template=template, input_variables=["context"])
+            answer = llm.completion(context, prompt=prompt)
             cur_result[model_type] = answer
         results.append(pd.DataFrame([cur_result]))
 
