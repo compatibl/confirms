4d34f05d7225b171abe05cf67a54e555214c6c16
Alexander Sokol <sokol@compatibl.com>
2023-09-24 10:54:43 -0400

    Refactor settings object.

--- confirms/core/settings.py
+++ confirms/core/settings.py
@@ -16,24 +16,28 @@ import os
 from pathlib import Path
 
 import openai
-from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
-from typing import Optional, ClassVar
+from typing import Optional
 from dotenv import load_dotenv
 
-# Load additional environment variables from .env file during import of this module
-load_dotenv()
 
-# Set OpenAI key explicitly because it does not automatically load the variables set by load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-
-@dataclass(init=False)
+@dataclass(slots=True, init=False)
 class Settings:
     """Default settings may be modified before the settings object is passed to the model."""
 
+    model_dir: str = field(default=None)
+    """Models are located in model_dir/model_name where model_name is either filename or directory name."""
+
     def __init__(self):
-        """Set default values."""
+        """Load settings from the environment variables and .env file (environment variables take precedence)."""
+
+        # Load additional environment variables from .env file during import of this module,
+        # Do not override the environment variables
+        load_dotenv(override=False)
+
+        # Set OpenAI key explicitly because it does not automatically load the variables set by load_dotenv()
+        # TODO: Pass OpenAI key to each method to allow code with different settings to run in parallel
+        openai.api_key = os.getenv("CONFIRMS_OPENAI_API_KEY")
 
         # Check environment variable first
         self.model_dir = os.getenv("CONFIRMS_MODEL_DIR")
@@ -41,14 +45,11 @@ class Settings:
         # If not set, use project_root/models
         if self.model_dir is None:
             self.model_dir = os.path.join(Path(os.path.dirname(__file__)).parent.parent, "models")
-            
+
         if not os.path.exists(self.model_dir):
             RuntimeError(f"Model directory {self.model_dir} does not exist.")
         if not os.path.isdir(self.model_dir):
             RuntimeError(f"Path specified for model directory {self.model_dir} is not a directory.")
-
-    model_dir: str = field(default=None)
-    """Models are located in model_dir/model_name where model_name is either filename or directory name."""
 
     def get_model_path(self, model_name: str, *, check_exists: Optional[bool] = True) -> str:
         """Get model path from model name using model_dir or its default value project_root/models,
@@ -56,12 +57,12 @@ class Settings:
 
         Models are located in model_dir/model_name where model_name is either filename or directory name,
         where model_dir is CONFIRMS_MODEL_DIR environment variable or project_root/models when not set.
-        
+
         - When model is a file, model_name should include extension
         - When model is a file, it should not
         """
+
         model_path = os.path.join(self.model_dir, model_name)
         if check_exists and not os.path.exists(model_path):
             raise RuntimeError(f"Model path {model_path} does not exist.")
         return model_path
-
