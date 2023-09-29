44244690172c5f17dcdd45d084070f60a98da549
Kateryna Dudko <katerynad@compatibl.com>
2023-09-29 06:01:35 -0700

    Extend tests for frequency.

--- tests/frequency/test_frequency_format.py
+++ tests/frequency/test_frequency_format.py
@@ -23,22 +23,57 @@ from confirms.core.llm.gpt_lang_chain_llm import GptLangChainLlm
 from confirms.core.llm.gpt_native_llm import GptNativeLlm
 from confirms.core.llm.llama_lang_chain_llm import LlamaLangChainLlm
 
+SIMPLE_CONTEXT = (
+    '```Effective Date: 15 June 2010.'
+    'Maturity Date: 15 June 2020.'
+    'Payment Frequency: Quarterly.```'
+)
 
-def run_frequency_extraction(*, result_name: str, temperature: Optional[float] = None):
+EXPLICIT_CONTEXT = (
+    '```Maturity Date: 18-July-2033. '
+    'Interest Payment Date: Interest payments shall be made quarterly on each 18th day of the months of '
+    'April, July, October and January, commencing October 18, 2023, and ending on the Maturity Date.```'
+)
+
+IMPLICIT_CONTEXT = (
+    '```Issue Date: 9 July 2009 (Settlement Date). '
+    'Maturity Date: 9 July 2013.'
+    'Interest Payment Dates: The 9th of each January, April, July, and October '
+    'commencing 9 October 2009 with a final payment on the Maturity Date.```'
+)
+
+VERBOSE_CONTEXT = (
+    '```Issue Date: On or about December 27, 2013.'
+    'Maturity Date and Term: On or about December 27, 2023, resulting in a term to maturity '
+    'of approximately 10 years. The $100 principal amount (the "Principal '
+    'Amount") will only be payable at maturity. For further information, see "Payments under the Notes". '
+    'Interest Payment Date: The first Interest payment, if any, shall be made on June 27, 2014, following '
+    'which Holders of the Notes will be entitled to receive semi-annual Interest payments, if any. '
+    'Subject to the occurrence of certain Extraordinary Events, Interest, if any, will be payable on the '
+    '27th day of June and December of each year that the Notes remain outstanding (each, an "Interest Payment'
+    ' Date") from and including June 27, 2014 to and including the Maturity Date. If any Interest Payment '
+    'Date is not a Business Day, it will be postponed to the next following Business Day.```'
+)
+
+PLAIN_TEMPLATE = (
+    "<s>[INST] Pay attention and remember information below, " 
+    "which will help to answer the question or imperative after the context ends. " 
+    "Context: {context}. " 
+    "According to only the information in the document sources provided within the context above, output payment "
+    "frequency only as one word and do not include any other text in your response. The payment frequency is[/INST]"
+)
+
+ONE_WORD_TEMPLATE = (
+    "<s>[INST] Pay attention and remember information below, " 
+    "which will help to answer the question or imperative after the context ends. " 
+    "Context: {context}. " 
+    "According to only the information in the document sources provided within the context above, "
+    "the payment frequency is[/INST]"
+)
+
+
+def run_frequency_extraction(*, template: str, context: str, result_name: str, temperature: Optional[float] = None):
     """Function completion for payment frequency extraction."""
-
-    context = (
-        "```Issue Date: 9 July 2009 (Settlement Date), Maturity Date: 9 July 2013, "
-        "Interest Payment Dates: The 9th of each January, April, July, and October "
-        "commencing 9 October 2009 with a final payment on the Maturity Date.```"
-    )
-    template = (
-        "<s>[INST] Pay attention and remember information below, "
-        "which will help to answer the question or imperative after the context ends. "
-        "Context: {context}. "
-        "According to only the information in the document sources provided within the context above, "
-        "the payment frequency is [/INST]"
-    )
 
     results = []
     for seed in range(1, 26):
@@ -65,14 +100,52 @@ def run_frequency_extraction(*, result_name: str, temperature: Optional[float] =
     df.to_csv(output_path, index=False)
 
 
-def test_frequency_extraction():
-    """Test function completion for payment frequency extraction with default model settings."""
-    run_frequency_extraction(result_name="frequency_implicit")
+def test_frequency_extraction_plain_simple():
+    """Test function completion for payment frequency extraction with default model settings
+    for plain template and simple context."""
+    run_frequency_extraction(template=PLAIN_TEMPLATE, context=SIMPLE_CONTEXT, result_name="frequency_plain_simple")
 
 
-def test_frequency_extraction_temp08():
-    """Test function completion for payment frequency extraction with temperature=0.8."""
-    run_frequency_extraction(result_name="frequency_implicit_temp08", temperature=0.8)
+def test_frequency_extraction_plain_explicit():
+    """Test function completion for payment frequency extraction with default model settings
+    for plain template and explicit context."""
+    run_frequency_extraction(template=PLAIN_TEMPLATE, context=EXPLICIT_CONTEXT, result_name="frequency_plain_explicit")
+
+
+def test_frequency_extraction_plain_implicit():
+    """Test function completion for payment frequency extraction with default model settings
+    for plain template and implicit context."""
+    run_frequency_extraction(template=PLAIN_TEMPLATE, context=IMPLICIT_CONTEXT, result_name="frequency_plain_implicit")
+
+
+def test_frequency_extraction_plain_verbose():
+    """Test function completion for payment frequency extraction with default model settings
+    for plain template and implicit context."""
+    run_frequency_extraction(template=PLAIN_TEMPLATE, context=VERBOSE_CONTEXT, result_name="frequency_plain_verbose")
+
+
+def test_frequency_extraction_word_simple():
+    """Test function completion for payment frequency extraction with default model settings
+    for one word template and simple context."""
+    run_frequency_extraction(template=ONE_WORD_TEMPLATE, context=SIMPLE_CONTEXT, result_name="frequency_word_simple")
+
+
+def test_frequency_extraction_word_explicit():
+    """Test function completion for payment frequency extraction with default model settings
+    for one word template and explicit context."""
+    run_frequency_extraction(template=ONE_WORD_TEMPLATE, context=EXPLICIT_CONTEXT, result_name="frequency_word_explicit")
+
+
+def test_frequency_extraction_word_implicit():
+    """Test function completion for payment frequency extraction with default model settings
+    for one word template and implicit context."""
+    run_frequency_extraction(template=ONE_WORD_TEMPLATE, context=IMPLICIT_CONTEXT, result_name="frequency_word_implicit")
+
+
+def test_frequency_extraction_word_verbose():
+    """Test function completion for payment frequency extraction with default model settings
+    for one word template and implicit context."""
+    run_frequency_extraction(template=ONE_WORD_TEMPLATE, context=VERBOSE_CONTEXT, result_name="frequency_word_verbose")
 
 
 def test_logit_processing():
